a0d15b2f2e2087e553924cdd4aac69bc03163bd6
João Pedro Maria <Joaopedromaria182@gmail.com>
2022-12-22 09:37:44 -0300

    concluido

---
+++ script.js
@@ -0,0 +1,8 @@
+function helloWord() {
+    window.alert("Hello, Word!")
+}
+
+function hello() {
+    var name = window.prompt("Olá! como você se chama?")
+    window.alert(`Prazer ${name}, fico feliz em te conhecer =)`)
+}
